7cdc6abcda0f2006bfb5b787825dc1b4623582ee
Arsen Slyusarchuk <arsen.slyusarchuk@perfectial.com>
2016-03-01 15:36:15 +0200

    Comments added

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -6,5 +6,12 @@ Template.postItem.helpers({
     var a = document.createElement('a');
     a.href = this.url;
     return a.hostname;
+  },
+  commentsCount: function() {
+    // console.log('parentTemplate');
+    // console.log(Template.parentData());
+    // console.log(Comments.find().count())
+    return Comments.find({postId: this._id}).count();
+    // return comments.find({postId: this._id}).count();
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,5 @@
+///////////////Posts///////////////////
+
 Meteor.publish('posts', function(options)  {
   check(options, {
     sort: Object,
@@ -9,4 +11,17 @@ Meteor.publish('posts', function(options)  {
 Meteor.publish('singlePost', function(id) {
   check(id, String);
   return id && Posts.find(id);
+});
+
+///////////////Comments///////////////////
+
+Meteor.publish('comments', function(postId) {
+  // check(postId, String);
+  // return Comments.find({postId: postId});
+  return Comments.find({});
+});
+
+Meteor.publish('comments_for_post', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
 });
